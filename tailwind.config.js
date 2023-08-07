2e9a8575c296cf0f3fb0ef5754e175d66b0bcf5d
SarangKumar <sarangkumar1578@gmail.com>
2023-08-08 01:13:56 +0530

    setup done for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				dark: {
+					100: "#292929",
+					200: "#121212",
+				},
+				text: {
+					100: "#CECECE",
+					200: "#676161",
+				},
+				background: "#1D1D1D",
+				primary: "#E40000",
+				secondary: "#FF5416",
+			},
+		},
+		fontFamily: {
+		// 	poppins: ['poppins', 'sans-serif'],
+		// 	sb: ['sb', 'serif'],
+		'handlee': ['Handlee'],
+		'poppins': ['Poppins'],
+		}
+	},
+	plugins: [],
+};
