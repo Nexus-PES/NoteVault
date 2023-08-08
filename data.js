022199f1afd894da3e9853d3e8e996e6cc9b45ff
SarangKumar <sarangkumar1578@gmail.com>
2023-08-08 16:10:03 +0530

    made the data.js file

---
+++ data.js
@@ -0,0 +1,27 @@
+export const featues = [
+    {
+        title: 'Features That Empowers',
+        description: 'Seamlessly create and edit notes with our user-friendly interface. Focus on your ideas while we take care of the formatting.'
+    },
+    {
+        title: 'Your Ideas, Your Style',
+        description: 'Personalize your note-taking experience with customizable themes and layouts. Make it truly yours.'
+    },
+    {
+        title: 'Security and Privacy',
+        description: 'We take your privacy seriously. Your notes are yours alone, and you decide who you share them with.'
+    },
+    {
+        title: 'Start Your Journey',
+        description: 'Join thousands of users who are already revolutionizing their note-taking experience with YourNoteHub.'
+    },
+]
+
+export const comments = [
+    {
+        ratings: 4.5,
+        imageUrl: '',
+        name: 'Name 1',
+        comment: 'Comment 1',
+    },
+]
