d3fec9e70a9bc10b4a713c8e0a210847ff53daf2
Dhruva <dhruvanayak1905@gmail.com>
2023-08-09 22:52:33 +0530

    changes in the page

---
+++ app/(notes)/[name]/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function (prop) {
+    const currentDate = new Date();
+  return (
+    <div class="box-content h-22 w-[250px] p-4 border-2 bg rounded-xl my-[10px] text-white text-md bg-[#343434]">
+        <p>{prop.title}</p>
+        <p>{prop.date}  date</p>
+    </div>
+  )
+}
--- app/(notes)/[name]/page.jsx
+++ app/(notes)/[name]/page.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+"use client"
 
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
 const UserPage = ({ params }) => {
+	var [greetings, setGreetings] = useState("");
+
+	var data = [
+		"Greetings, my friend! How's life treating you?",
+		"Salutations! What's new in your world?",
+		"Ahoy there! Long time no chat.",
+		"Top of the morning to you! Ready for some conversation?",
+		"Well met! What's the latest scoop?",
+		"Hey you! How's everything on your end?",
+		"Hola amigo! It's time for another catch-up.",
+		"Hey sunshine! What's been keeping you busy?",
+		"Yoohoo! Let's have a splendid chat, shall we?",
+		"Hey, hey! Ready to dive into some discussions?"
+	];
+
+	useEffect(() => {
+		if (data.length > 0) {
+			const randomIndex = Math.floor(Math.random() * data.length);
+			const randomGreeting = "Hi " + params.name + "!" + data[randomIndex];
+
+			setGreetings(randomGreeting);
+		}
+	}, [data]);
+
+	const info = [
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		{ title: 'Title 1' },
+		
+	];
+
 	return (
 		<div>
-			<h1 className="text-transparent bg-gradient-to-r from-primary to-secondary bg-clip-text">
-				Hi {params.name}
-			</h1>
+			<div className="flex justify-center items-center">
+				<div className="my-[50px]">
+					<h1 className="text-white text-[34px] font-semibold font-poppins leading-[20px] ">
+						{greetings}
+					</h1>
+				</div>
+			</div>
+			<div className="p-6">
+				<div className="flex flex-wrap justify-evenly">
+					{info.map((item, index) => (
+						<div key={index} className="">
+							<Card title={item.title} date="" />
+						</div>
+					))}
+				</div>
+			</div>
+
+
 		</div>
 	);
 };
