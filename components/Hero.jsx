76dd6f91a376f5b32e7b1d931f6fdd2bcf004bc2
Nistha Singh <nistha20003@gmail.com>
2023-08-10 17:23:08 +0530

    heading

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Button from "./Button";
-
 const Hero = () => {
 	return (
-		<main className="container px-8 gap-3 py-24 text-white flex flex-col items-center">
+		<main className="container px-8 gap-3 py-24 text-white flex flex-col">
 			<div>
 				<h1 className="text-center text-wrap-balance text-heading sm:leading-[64px] leading-9 font-poppins font-semibold">
 					A Simple way to
@@ -18,9 +17,24 @@ const Hero = () => {
 			<p className="text-xs sm:text-sm md:text-base text-center tracking-wide text-text-100 font-medium">
 				Capture, Create, Connect: Your Personal Note-Making Journey
 			</p>
-			<div className="flex gap-4 my-4 md:my-9">
+			<div className="flex justify-center">
+
+			<div className="flex gap-4 my-4 md:my-9 justify-center">
 				<Button>Make Notes</Button>
 				<Button type="glory">Join Us</Button>
+			</div>
+			</div>
+			<br></br>
+			<div className="flex justify-center">
+
+			<h1 className="font-['Poppins'] flex items-center text-7xl sm:text-9xl font-[900]">NoteVault</h1>
+			</div>
+			<br></br>
+			<div className="flex m-[8rem]">
+				<div className="font-['Poppins'] font-[600]  text-[2.16rem] ">Why <span className="text-[#E40000]">NoteVault</span>?</div>
+				<div>
+					<img src="kybernetwork.svg"></img>
+				</div>
 			</div>
 		</main>
 	);
