75935f6118618c6e340c2c9ef24e160f8b63d0f2
ClayTsunami <claytsunami@gmail.com>
2023-08-10 19:14:46 +0530

    Made other styling changes and added links to the cards

--- app/(notes)/[name]/[note]/page.jsx
+++ app/(notes)/[name]/[note]/page.jsx
@@ -22,7 +22,7 @@ const Notes = ({ params }) => {
 				defaultValue={
 					decodeURI(params.note)
 				}
-				className="text-2xl md:text-4xl font-bold block w-full rounded-md border-0 px-3.5 py-2 text-text-100 sm:text-sm sm:leading-6 bg-transparent focus:ring-0 focus:border-2 focus:border-white"
+				className="text-2xl md:text-4xl font-bold block w-full rounded-md px-3.5 py-2 text-text-100 sm:text-sm sm:leading-6 bg-transparent focus:ring-0 border-0 focus:border-2 focus:border-white"
 				maxLength={100}
 				minLength={3}
 				required
