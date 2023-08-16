298725eb956fda21a06fa2565c7edfc8a21af52b
ClayTsunami <claytsunami@gmail.com>
2023-08-16 21:42:38 +0530

    Cleaning up

--- app/(notes)/[name]/[note]/Footer.jsx
+++ app/(notes)/[name]/[note]/Footer.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function Footer(props) {
-  return (
-    <div className='fixed gap-4  flex left-0 bottom-0 bg-dark-100 w-full h-9 px-2 py-2'>
-        <div className='flex gap-4 text-xs grow font-poppins'>
-        <p className='mx-1'>.....words</p>
-        <p className='mx-1'>Created on:...........</p>
-        <p className='mx-1'>Last Modified on:.............</p>
-        </div>
-        <div className='flex grow text-xs flex-row-reverse font-poppins'>
-        <p className=''>Recording <span className='text-secondary'>............</span></p>
-        </div>
-    </div>
-  )
+function Footer({ words, saving }) {
+	return (
+		<div className="fixed gap-4  flex left-0 bottom-0 bg-dark-100 w-full h-9 px-2 py-2">
+			<div className="flex gap-4 text-xs grow font-poppins">
+				<p className="mx-1">
+					{words} words
+				</p>
+				<p className="mx-1">
+					Created
+					on:...........
+				</p>
+				<p className="mx-1">
+					Last Modified
+					on: {saving}
+				</p>
+			</div>
+			<div className="flex grow text-xs flex-row-reverse font-poppins">
+				<p className="">
+					Recording{" "}
+					<span className="text-secondary">
+						............
+					</span>
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
