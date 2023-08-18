29668c94fce5ce4715f28707ca755b80078b9ae1
Nistha Singh <nistha20003@gmail.com>
2023-08-18 22:39:44 +0530

    finalhomepage

--- app/page.js
+++ app/page.js
@@ -2,17 +2,12 @@ import Hero from "../components/Hero";
 import Comment from "../components/Comment";
 import Features from "../components/Features";
 import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <div className="min-h-screen text-text-100 bg-background mx-5">
       <Hero />
-=======
-	return (
-		<div className="min-h-screen text-text-100 bg-background">
-			<Hero />
->>>>>>> fc5a7c117a70aecdc4eb5d405a6b29f97629ef1e
 
 			{/* Features */}
 			<Features />
