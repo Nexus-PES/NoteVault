93968b428b18a05dc878cb4ed1bb53999329df4e
Sarang Kumar <sarangkumar1578@gmail.com>
2023-08-27 19:55:03 +0530

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
 	title: "Note Vault",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className="bg-background">{children}</body>
+			<body className="bg-background">{children}  <Analytics /></body>
 		</html>
 	);
 }
