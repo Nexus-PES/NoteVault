796512eb0104d0ee9a3146e5ab14553abe76a511
Sarang Kumar <sarangkumar1578@gmail.com>
2023-08-29 09:57:09 +0530

    prod changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-const production = true;
+const production = true
 
 const handler = NextAuth({
 	providers: [
