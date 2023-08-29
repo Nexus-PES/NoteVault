e51a85c84261da39e553adb21a755f7d9735cb01
Sarang Kumar <sarangkumar1578@gmail.com>
2023-08-29 12:23:13 +0530

    modified signin ui + resolved prod issues

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,24 +7,55 @@ import iconic from "public/images/kybernetwork.svg";
 import features from "../data";
 import Comment from "../components/Comment";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { BsGithub } from "react-icons/bs";
 
 const Hero = () => {
 	const { data: session } = useSession();
 	let avatar;
 	if (session) {
-		console.log(session);
 		avatar = session.user.image;
 	}
 	return (
 		<main className="container pt-24 text-white flex flex-col justify-center">
-		{session && 
-			<Image
-						src={avatar}
-						alt="profile picture"
-						width={100}
-						height={100}
-						className="h-9 w-9 sm:w-10 sm:h-10 rounded-full flex-wrap fixed top-5 left-10"
-					/>}
+			<div className="flex items-center justify-between mb-5 -mt-8">
+				<div className="flex gap-x-4 items-center">
+					<a href="https://github.com/Nexus-PES/NoteVault">
+						<BsGithub className="h-6 w-6 inline-block" />
+						<span className="px-2 text-xs text-text-100 hidden sm:inline-block font-semibold">
+							Visit, fork, star on GitHub
+						</span>
+					</a>
+				</div>
+				<div>
+					{session ? (
+						<div className="flex gap-x-4 items-center">
+							<button
+								onClick={() => signOut("github")}
+								className="inline-block text-xs text-center text-dark-100 bg-white font-semibold hover:underline hover:bg-stone-100 rounded px-4 py-2 transition"
+							>
+								Sign Out
+							</button>
+							<Image
+								src={avatar}
+								alt="profile picture"
+								width={100}
+								height={100}
+								className="h-9 w-9 sm:w-10 sm:h-10 rounded-full  hover:bg-stone-100 transition-colors"
+							/>
+						</div>
+					) : (
+						<>
+							<button
+								onClick={() => signIn("github")}
+								className="inline-block text-xs text-center text-white bg-gradient-to-br from-primary to-secondary font-semibold hover:underline hover:bg-stone-100 rounded px-4 py-2 transition"
+							>
+								Sign In
+							</button>
+						</>
+					)}
+				</div>
+			</div>
+
 			<div className="-space-y-2">
 				<h1 className="text-center text-wrap-balance text-clamp-hero font-poppins font-semibold">
 					A Simple way to
@@ -48,25 +79,19 @@ const Hero = () => {
 				{session ? (
 					<>
 						<Button
+						type="glory"
 							href={`/${session.user.name.split(" ").join("")}`}
 						>
 							Make Notes
 						</Button>
-						<Button
-							type="glory"
-							onClick={() => signOut("github")}
-						>
-							Sign Out
-						</Button>
 					</>
 				) : (
-
-				<Button
-					type="glory"
-					onClick={() => signIn("github")}
-				>
-					Join Us
-				</Button>
+					<Button
+						type="glory"
+						onClick={() => signIn("github")}
+					>
+						Join Us
+					</Button>
 				)}
 			</div>
 			<br></br>
