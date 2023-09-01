8209875be966a5a3edd194995d2ea66d90f3b075
Sarang Kumar <sarangkumar1578@gmail.com>
2023-09-02 01:47:19 +0530

    fixed pwa

--- app/layout.js
+++ app/layout.js
@@ -4,24 +4,12 @@ import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
 	title: "Note Vault",
-	description: "A Simple way to create Notes",
+	description: "NoteVault is a versatile note-taking platform designed to enhance organization and idea connectivity. It empowers users to create, link, and manage notes efficiently, fostering better understanding and exploration of their thoughts.",
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<link
-				rel="manifest"
-				href="../manifest.json"
-			/>
-			<link
-				rel="apple-touch-icon"
-				href="/icons/icon-48.png"
-			></link>
-			<meta
-				name="theme-color"
-				content="#fff"
-			/>
 			<body className="bg-background">
 				<Provider>
 					{children}
---
+++ app/manifest.js
@@ -0,0 +1,63 @@
+export default function manifest() {
+	return {
+		name: "NoteVault App",
+		short_name: "Next.js App",
+		description: "NoteVault is a versatile note-taking platform designed to enhance organization and idea connectivity. It empowers users to create, link, and manage notes efficiently, fostering better understanding and exploration of their thoughts.",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#fff",
+		theme_color: "#fff",
+		icons: [
+			{
+				src: "/icons/icon-48.png",
+				sizes: "48x48",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-72.png",
+				sizes: "72x72",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-96.png",
+				sizes: "96x96",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-120.png",
+				sizes: "120-120",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-128.png",
+				sizes: "128x128",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-144.png",
+				sizes: "144x144",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-180.png",
+				sizes: "180x180",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-384.png",
+				sizes: "384x384",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	};
+}
