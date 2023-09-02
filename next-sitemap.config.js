79c092d9d313ef8db2b324e87894f8ee04cd6b0f
Sarang Kumar <sarangkumar1578@gmail.com>
2023-09-02 17:56:02 +0530

    made the sitemap functional - seo optimised

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
     siteUrl:  process.env.SITE_URL,
     generateRobotsTxt: true,
-    robotsTxtOptions: [
-        {userAgent: '*', allow: '/'},
-        {userAgent: '*', disallow: '/demo'},
-        {userAgent: '*', disallow: '/test'},
-],
     sitemapSize: 7000,
     changefreq: 'daily',
     exclude: ['/demo', '/test'],
